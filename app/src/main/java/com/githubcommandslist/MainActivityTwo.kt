bf95b050bb197e7604f83a9c34ec7a80f3a868eb
yerrabolu <yerrabolunavyareddy@gmail.com>
2024-10-01 16:50:32 +0530

    2nd Commit to Yerrabolu branch added MainactivityTwo  File

---
+++ app/src/main/java/com/githubcommandslist/MainActivityTwo.kt
@@ -0,0 +1,19 @@
+package com.githubcommandslist
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.githubcommandslist.databinding.ActivityMainTwoBinding
+
+class MainActivityTwo : AppCompatActivity() {
+    lateinit var binding: ActivityMainTwoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainTwoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // This is 2nd commit to yerrabolu branch
+    }
+}
