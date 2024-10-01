b84e8ea00bd235585a89890775684f224b167c9a
parudeveloper <paramesh.developer@gmail.com>
2024-10-01 16:34:56 +0530

    Mallangi 2nd commit

--- app/src/main/java/com/githubcommandslist/MainActivityOne.kt
+++ app/src/main/java/com/githubcommandslist/MainActivityOne.kt
@@ -1,10 +1,7 @@
 package com.githubcommandslist
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.githubcommandslist.databinding.ActivityMainOneBinding
 
 class MainActivityOne : AppCompatActivity() {
@@ -14,5 +11,7 @@ class MainActivityOne : AppCompatActivity() {
         binding= ActivityMainOneBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // This is added by Mallangi and committing
+
+        // Mallangi 2nd commit
     }
 }
